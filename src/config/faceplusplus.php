5ab2f21a34ddd3b0cac3164a66009ebcfa57ba3d
Richard <richard_tianke@qq.com>
2018-10-12 15:40:44 +0800

    Upgrade json

--- src/config/faceplusplus.php
+++ src/config/faceplusplus.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'api_key'    => env('FPP_API_KEY', 'face-plus-plus-app-key'),
-    'api_secret' => env('FPP_API_SECRET', 'face-plus-plus-app-secret'),
+    'api_key'    => env('FPP_API_KEY', 'face-plus-plus-api-key'),
+    'api_secret' => env('FPP_API_SECRET', 'face-plus-plus-api-secret'),
 ];
